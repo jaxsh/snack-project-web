62bb160d68d36870e73e0b56fbc1756f63061b78
Jax Jiang <jiang.tao.sh.cn@gmail.com>
2026-05-10 08:04:45 +0900

    feat: delete unused code

--- config/routes.ts
+++ config/routes.ts
@@ -17,29 +17,6 @@ export default [
     component: './Welcome',
   },
   {
-    path: '/admin',
-    name: '管理页',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: '/admin',
-        redirect: '/admin/sub-page',
-      },
-      {
-        path: '/admin/sub-page',
-        name: '二级管理页',
-        component: './Admin',
-      },
-    ],
-  },
-  {
-    name: '查询表格',
-    icon: 'table',
-    path: '/list',
-    component: './table-list',
-  },
-  {
     path: '/',
     redirect: '/welcome',
   },
